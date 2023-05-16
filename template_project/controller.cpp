12d8fe12a9fe6b8178f6baa6f3a0b1d4fadacaf2
Dea Dressel <ddressel@stanford.edu>
2023-05-16 15:46:06 -0700

    controller update, stanbot joints renamed

--- template_project/controller.cpp
+++ template_project/controller.cpp
@@ -24,7 +24,7 @@ void sighandler(int){runloop = false;}
 #include "redis_keys.h"
 
 // Location of URDF files specifying world and robot information
-const string robot_file = "./resources/panda_arm.urdf";
+const string robot_file = "./resources/stanbot.urdf";
 
 enum State 
 {
@@ -59,17 +59,17 @@ int main() {
 	MatrixXd N_prec = MatrixXd::Identity(dof, dof);
 
 	// pose task
-	const string control_link = "link7";
-	const Vector3d control_point = Vector3d(0, 0, 0.07);
-	auto posori_task = new Sai2Primitives::PosOriTask(robot, control_link, control_point);
-	posori_task->_use_interpolation_flag = true;
-	posori_task->_use_velocity_saturation_flag = true;
+	// const string control_link = "link7";
+	// const Vector3d control_point = Vector3d(0, 0, 0.07);
+	// auto posori_task = new Sai2Primitives::PosOriTask(robot, control_link, control_point);
+	// posori_task->_use_interpolation_flag = true;
+	// posori_task->_use_velocity_saturation_flag = true;
 
-	VectorXd posori_task_torques = VectorXd::Zero(dof);
-	posori_task->_kp_pos = 400.0;
-	posori_task->_kv_pos = 40.0;
-	posori_task->_kp_ori = 400.0;
-	posori_task->_kv_ori = 40.0;
+	// VectorXd posori_task_torques = VectorXd::Zero(dof);
+	// posori_task->_kp_pos = 400.0;
+	// posori_task->_kv_pos = 40.0;
+	// posori_task->_kp_ori = 400.0;
+	// posori_task->_kv_ori = 40.0;
 
 	// joint task
 	auto joint_task = new Sai2Primitives::JointTask(robot);
@@ -81,8 +81,10 @@ int main() {
 	joint_task->_kv = 40.0;
 
 	VectorXd q_init_desired(dof);
-	q_init_desired << -30.0, -15.0, -15.0, -105.0, 0.0, 90.0, 45.0;
-	q_init_desired *= M_PI/180.0;
+
+	// chair pose
+	q_init_desired << 0.454621,-1.599603,1.473881,-0.171505,-0.235412,0.327871,0.210719,-1.404628,1.601027,-0.508144,-0.008615,-0.098502,0.336433,-3.321765,-0.447239,0.059882,-0.036452,0.422319,0.770947,1.522959,-3.299092,-0.266798,-0.112313,-0.106552,0.390400,-0.107298,-0.506217,-0.787140,0.192233,0.269256;
+	// q_init_desired *= M_PI/180.0;
 	joint_task->_desired_position = q_init_desired;
 
 	// containers
@@ -135,10 +137,10 @@ int main() {
 			if ( (robot->_q - q_init_desired).norm() < 0.15 ) {
 				cout << "Posture To Motion" << endl;
 				joint_task->reInitializeTask();
-				posori_task->reInitializeTask();
-				robot->position(ee_pos, control_link, control_point);
-				posori_task->_desired_position = ee_pos - Vector3d(-0.1, -0.1, 0.1);
-				posori_task->_desired_orientation = AngleAxisd(M_PI/6, Vector3d::UnitX()).toRotationMatrix() * posori_task->_desired_orientation;
+				// posori_task->reInitializeTask();
+				// robot->position(ee_pos, control_link, control_point);
+				// posori_task->_desired_position = ee_pos - Vector3d(-0.1, -0.1, 0.1);
+				// posori_task->_desired_orientation = AngleAxisd(M_PI/6, Vector3d::UnitX()).toRotationMatrix() * posori_task->_desired_orientation;
 				// posori_task->_desired_orientation = AngleAxisd(0.0000000000000001, Vector3d::UnitX()).toRotationMatrix() * posori_task->_desired_orientation;
 
 				state = MOTION;
@@ -146,14 +148,15 @@ int main() {
 		} else if (state == MOTION) {
 			// update task model and set hierarchy
 			N_prec.setIdentity();
-			posori_task->updateTaskModel(N_prec);
-			N_prec = posori_task->_N;
+			// posori_task->updateTaskModel(N_prec);
+			// N_prec = posori_task->_N;
 			joint_task->updateTaskModel(N_prec);
 
 			// compute torques
-			posori_task->computeTorques(posori_task_torques);
+			// posori_task->computeTorques(posori_task_torques);
 			joint_task->computeTorques(joint_task_torques);
-			command_torques = posori_task_torques + joint_task_torques;
+			// command_torques = posori_task_torques + joint_task_torques;
+			command_torques = joint_task_torques;
 		}
 
 		// execute redis write callback
